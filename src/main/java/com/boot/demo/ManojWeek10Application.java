867784f3de0d1cc336d41e8130a022fe3273399e
Manoj R <r.manoj@hcl.com>
2021-12-11 13:55:38 +0530

    first commit

---
+++ src/main/java/com/boot/demo/ManojWeek10Application.java
@@ -0,0 +1,14 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManojWeek10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManojWeek10Application.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
